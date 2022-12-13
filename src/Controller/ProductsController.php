a6e84c1736dedba12caaef6cb0110fe28fbacfab
Sumanai <sumanai@ya.ru>
2022-12-13 15:42:24 +0400

    feat(product): add pagination

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -4,18 +4,21 @@ namespace App\Controller;
 
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductsController extends AbstractController
 {
     #[Route('/products', name: 'app_products')]
-    public function index(ProductRepository $products): Response
+    public function index(Request $request, ProductRepository $products): Response
     {
-        $productsList = $products->findAll();
+        $page = $request->query->getInt('page', 1);
+        $productsPaginator = $products->findWithPages($page);
 
         return $this->render('products/index.html.twig', [
-            'products' => $productsList,
+            'products' => $productsPaginator->getIterator(),
+            'count' => $productsPaginator->count(),
         ]);
     }
 }
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -4,6 +4,8 @@ namespace App\Repository;
 
 use App\Entity\Product;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Tools\Pagination\CountWalker;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -39,28 +41,51 @@ class ProductRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Product[] Returns an array of Product objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findWithPages(int $page = 1): Paginator
+    {
+        $currentPage = max(1, $page);
+        $firstResult = ($currentPage - 1) * 10;
 
-//    public function findOneBySomeField($value): ?Product
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+        $queryBuilder = $this->createQueryBuilder('p')
+            ->innerJoin('p.category', 'c')
+            ->addSelect('c')
+            ->orderBy('p.id', 'DESC');
+
+        $query = $queryBuilder
+            ->setFirstResult($firstResult)
+            ->setMaxResults(10)
+            ->getQuery();
+
+        // Make good query
+        $query->setHint(CountWalker::HINT_DISTINCT, false);
+        $paginator = new Paginator($query, false);
+        $paginator->setUseOutputWalkers(false);
+
+        return $paginator;
+    }
+
+    //    /**
+    //     * @return Product[] Returns an array of Product objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('p')
+    //            ->andWhere('p.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('p.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Product
+    //    {
+    //        return $this->createQueryBuilder('p')
+    //            ->andWhere('p.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
